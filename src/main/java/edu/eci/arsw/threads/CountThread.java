c1ec2727e9a288313dcf377394bda8b571822428
StivenVanegas <nevits28@hotmail.com>
2021-01-20 09:56:27 -0500

    Parte 1 Clases CountThread

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -9,6 +9,21 @@ package edu.eci.arsw.threads;
  *
  * @author hcadavid
  */
-public class CountThread {
+public class CountThread implements Runnable{
     
+	private int a;
+	private int b;
+	
+    public CountThread(int a,int b){
+        this.a = a;
+        this.b = b;
+    }
+	
+    @Override
+    public void run() {
+        for (int i=a;i<=b;i++){
+            System.out.println(i);
+        }
+    }
+	
 }
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -12,7 +12,14 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
-        
+		
+		Thread thread1 = new Thread(new CountThread(0,99));
+        Thread thread2 = new Thread(new CountThread(99,199));
+        Thread thread3 = new Thread(new CountThread(200,299));
+		
+		thread1.start();
+		thread2.start();
+		thread3.start();
     }
     
 }
